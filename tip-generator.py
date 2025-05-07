0c65a8ca1470d142e771633773f6e1ae9669269d
Kevin Yechan An <109259015+kevinan1924@users.noreply.github.com>
2025-05-07 15:33:20 -0500

    Update tip-generator.py

--- tip-generator.py
+++ tip-generator.py
@@ -2,7 +2,7 @@
 print("Welcome to the tip calculator.")
 print("What was the total bill?")
 total = input()
-print("What percentage tip would you like to give? 10, 12, or 15?")
+print("What percentage tip would you like to give? 10, 12, 15, etc?")
 tip = input()
 print("How many people to split the bill?")
 people_split = input()
